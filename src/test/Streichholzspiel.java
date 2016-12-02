a67f8c16d891d1e76d9145b6a204641652a3dba9
Michael <m.gruending@ostfalia.de>
2016-12-02 13:11:47 +0100

    alles wieder hoch

---
+++ src/test/Streichholzspiel.java
@@ -0,0 +1,17 @@
+package test;
+public class Streichholzspiel { 
+
+	public static void main(String[] args) {
+
+		Fenster Fenster=new Fenster();
+
+		Spiellogik spiel=new Spiellogik();
+
+		Fenster.erzeugeOberflaeche();
+
+		spiel.nenneStartHoelzer();
+
+		}
+
+}
+
