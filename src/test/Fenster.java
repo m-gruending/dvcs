90d730b2b6438eb0c1dbaed7d5422bc8549b3547
Michael <m.gruending@ostfalia.de>
2016-12-09 14:48:40 +0100

    aenderung

--- src/test/Fenster.java
+++ src/test/Fenster.java
@@ -3,7 +3,8 @@ import java.awt.Frame;
 import java.awt.Button; 
 import java.awt.Panel; 
 import java.awt.event.ActionEvent; 
-import java.awt.event.ActionListener; 
+import java.awt.event.ActionListener;
+//änderung
 
 public class Fenster implements ActionListener{ 
 	Frame oberflaeche= new Frame(); 
